60b1c25c639054c9b2b8e6763fd7a6a1fd70429a
ankitlakra <ankitlakra157@gmail.com>
2023-10-07 20:09:13 +0530

    preview

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -10,8 +10,9 @@ export default function Page() {
   const [doctype, setDoctype] = useState('');
   const [docid, setDocid] = useState('');
   const [docuri, setDocuri] = useState(null);
-  const [msg, setMsg] = useState('');
-  
+  const [errmsg, setErrorMsg] = useState('');
+  const [copySuccess, setCopySuccess] = useState(false);
+
   useEffect(() => {
     // Access the current URL
     const currentUrl = window.location.href;
@@ -31,26 +32,65 @@ export default function Page() {
             setDoctype(doc.data().doctype);
             setDocuri(doc.data().docUri);
           } else {
-            console.log('Document not found');
+            setErrorMsg('Document not found');
           }
         })
         .catch(function (error) {
-          setMsg(error);
+          setErrorMsg(error);
           console.log('Error fetching document:', error);
         });
     }
   }, [docid]);
-  const datafile = [
-    { uri: docuri },
-  ];
 
-  console.log(datafile);
+  const handleDownload = () => {
+    // You can open the file in a new tab/window for viewing or download
+    if (docuri) {
+      window.open(docuri, '_blank');
+    }
+  };
+  const handleCopyLink = () => {
+    if (docuri) {
+      navigator.clipboard.writeText(docuri).then(
+        function () {
+          setCopySuccess(true);
+        },
+        function (err) {
+          console.error('Unable to copy link: ', err);
+        }
+      );
+    }
+  };
 
   return (
     <div>
+      {copySuccess && <><div className="alert alert-success">
+        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <span>Link copied to clipboard!</span>
+      </div></>}
+      {errmsg && <><div className="alert alert-error">
+        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <span>{errmsg}</span>
+      </div></>}
+
       <p>Title {title}</p>
-      <DocViewer documents={datafile} pluginRenderers={DocViewerRenderers} />
-      {msg}
+      <p>Description {desc}</p>
+
+      <button className="btn btn-outline btn-info" onClick={handleDownload}>Download</button>
+      <button className="btn btn-outline btn-success" onClick={handleCopyLink}>Copy Link</button>
+
+      <div className="m-20">
+        {docuri && (
+          <iframe
+            src={docuri}
+            width="100%"
+            height="600px"
+            frameBorder="0"
+            title="Document Viewer"
+          ></iframe>
+        )}
+      </div>
+
+
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   daisyui: {
-    themes: ["synthwave"],
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#6e35bf",
+
+          "secondary": "#f2e021",
+
+          "accent": "#eda3bf",
+
+          "neutral": "#292c38",
+
+          "base-100": "#f1ebf5",
+
+          "info": "#2779ec",
+
+          "success": "#169c78",
+
+          "warning": "#fbcb6a",
+
+          "error": "#f6555b",
+        },
+      },
+    ],
   },
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
