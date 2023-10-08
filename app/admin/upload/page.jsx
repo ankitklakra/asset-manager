c73333406a38ab223c5e813dead619a23756cce9
Govind <Govindpurty350@gmail.com>
2023-10-08 19:27:10 +0530

    admin panel update

--- app/admin/upload/page.jsx
+++ app/admin/upload/page.jsx
@@ -168,28 +168,47 @@ export default function Admin() {
     return (
         <main>
             <div className="m-5" >
-                <br />
+               
 
                
-                <nav className="flex py-6 " aria-label="Breadcrumb">
+            <nav className="flex py-6 ps-8" aria-label="Breadcrumb">
   <ol className="inline-flex items-center space-x-1 md:space-x-3">
     <li className="inline-flex items-center">
-      <a href="/admin" className="inline-flex items-center text-sm font-medium text-black-700 hover:text-blue-600 dark:text-black-400 dark:hover:text-black">
+      <a href="/" className="inline-flex items-center text-sm font-medium text-black-700 hover:text-blue-600 dark:text-black-400 dark:hover:text-black">
         <svg className="w-3 h-3 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
           <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
         </svg>
-        Admin
+        Home
       </a>
     </li>
     <li aria-current="page" className="flex items-center">
       <svg className="w-3 h-3 text-black-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
       </svg>
+      <a href="/profile" className="inline-flex items-center text-sm font-medium text-black-700 hover:text-blue-600 dark:text-black-400 dark:hover:text-black">
+      <span className="ml-1 text-sm font-medium text-black-500 md:ml-2 dark:text-black-400">Profile</span>
+      </a>
+    </li>
+    <li aria-current="page" className="flex items-center">
+      <svg className="w-3 h-3 text-black-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
+      </svg>
+      <a href="/admin" className="inline-flex items-center text-sm font-medium text-black-700 hover:text-blue-600 dark:text-black-400 dark:hover:text-black">
+      <span className="ml-1 text-sm font-medium text-black-500 md:ml-2 dark:text-black-400">Admin</span>
+      </a>
+    </li>
+
+    <li aria-current="page" className="flex items-center">
+      <svg className="w-3 h-3 text-black-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
+      </svg>
+     
       <span className="ml-1 text-sm font-medium text-black-500 md:ml-2 dark:text-black-400">Upload</span>
+      
     </li>
   </ol>
 </nav>
-<h1 className='flex justify-center'> ADMIN PANEL</h1>
+<h1 className='flex justify-center'> UPLOAD RESOURCES </h1>
 
                 {/* <form className="form-group" onSubmit={handleLogout}>
                     <h1>
@@ -250,7 +269,7 @@ export default function Admin() {
                         </>
                     )} */}
                     <br />
-                    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <div className='py-8'style={{ display: 'flex', justifyContent: 'flex-Start' }}>
                         <button type="submit" className="btn btn-success btn-md">
                             SUBMIT
                         </button>
